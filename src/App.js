8edc5895275aee95a442d3af25fabfc115c619e5
Abhilash <abhilash.vc888@gmail.com>
2025-03-29 13:03:34 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import photo1 from "./image/photo-1.avif";
+import photo2 from "./image/photo-2.avif";
+import photo3 from "./image/photo-3.avif";
+import photo4 from "./image/photo-4.avif";
+import "./index.css";
 
-function App() {
+const App = () => {
+  const parentDIv = useRef();
+  let i = 0;
+  let interval = setInterval(() => {
+    if (i == "-75") {
+      i = 0;
+    } else {
+      i = i - 25;
+    }
+    parentDIv.current.style.transform = `translate(${i}%,0px)`;
+    parentDIv.current.style.transition = "all 0.5s linear 0s";
+  }, 3000);
+  const prevHandeler = () => {
+    clearInterval(interval);
+    if (i == 0) {
+      i = -75;
+    } else {
+      i = i + 25;
+    }
+
+    parentDIv.current.style.transform = `translate(${i}%,0px)`;
+    parentDIv.current.style.transition = "all 0.3s linear 0s";
+  };
+  const nextHandeler = () => {
+    clearInterval(interval);
+    if (i == "-75") {
+      i = 0;
+    } else {
+      i = i - 25;
+    }
+
+    parentDIv.current.style.transform = `translate(${i}%,0px)`;
+    parentDIv.current.style.transition = "all 0.3s linear 0s";
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        style={{
+          height: "600px",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <div
+          style={{
+            height: "85%",
+            width: "33%",
+            overflow: "hidden",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div
+            style={{ height: "100%", width: "400%", display: "flex" }}
+            ref={parentDIv}
+          >
+            <div style={{ width: "100%", height: "100%" }}>
+              <img
+                src={photo1}
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </div>
+            <div style={{ width: "100%", height: "100%" }}>
+              <img
+                src={photo2}
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </div>
+            <div style={{ width: "100%", height: "100%" }}>
+              <img
+                src={photo3}
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </div>
+            <div style={{ width: "100%", height: "100%" }}>
+              <img
+                src={photo4}
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            height: "5%",
+            width: "33%",
+          }}
+        >
+          <button type="button" className="btnHover" onClick={prevHandeler}>
+            Prev
+          </button>
+          <button
+            type="button"
+            style={{ marginLeft: "2%" }}
+            className="btnHover"
+            onClick={nextHandeler}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
